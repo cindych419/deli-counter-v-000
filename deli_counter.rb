7ebdf69e3329035f39bf9ebb2db665ee8048ad7f
cindych419@gmail.com <cindych419@gmail.com>
2018-05-01 22:18:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,6 +11,6 @@ def line(deli)
 end
 def take_a_number(deli, name)
   deli.each_with_index do |person,i|
-    puts "Welcome, #{name}. You are number #{array.size+1} in line."
+    puts "Welcome, #{name}. You are number #{deli.size+1} in line."
   end
 end
