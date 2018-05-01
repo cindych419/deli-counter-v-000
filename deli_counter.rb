59faa7b2141d4598746b3ac2afd966b3b053a9c9
cindych419@gmail.com <cindych419@gmail.com>
2018-05-01 22:07:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(deli)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli.each.with_index(1) do |person,i|
-    current_line << " #{i}. #{person}"
+    deli.each_with_index do |person,i|
+    current_line << " #{i+1}. #{person}"
     end
     puts current_line
   end
